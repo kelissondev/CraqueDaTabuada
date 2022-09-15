a53b8c894cf72806bb717a13102542d87212bafd
Kelisson Rodrigues <62763625+kelissondev@users.noreply.github.com>
2022-09-15 16:23:14 -0300

    Add files via upload

---
+++ CraqueDaTabuada.py
@@ -0,0 +1,8 @@
+numero = int (input ("Digite um número para saber sua tabuada: "))
+
+for i in range (1, 11) :
+    print (i , "x" , numero, "=", 1*numero)
+   
+print ("Obrigado por utilizar e estuda, viu!")
+
+input()
